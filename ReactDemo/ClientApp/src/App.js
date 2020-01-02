5978b1611e5eaa7f1f0673684ed00a90f489409b
ashish <ashrestha@pcs.com.np>
2020-01-02 10:32:14 +0545

    Initial commit

---
+++ ReactDemo/ClientApp/src/App.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import { Route } from 'react-router';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Counter from './components/Counter';
+import FetchData from './components/FetchData';
+import Students from './components/Students';
+
+export default () => (
+  <Layout>
+    <Route exact path='/' component={Home} />
+    <Route path='/counter' component={Counter} />
+    <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
+    <Route path='/students' component={Students} />
+  </Layout>
+);
---
+++ ReactDemo/ClientApp/src/components/Students.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import { connect } from 'react-redux';
+
+
+const Home = props => (
+    <div>
+        <h2>Student Registration</h2>
+       </div>
+);
+
+export default connect()(Home);
