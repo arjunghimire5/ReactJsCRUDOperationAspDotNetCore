0011858292b7421c5f34a4f9fe6db3226ed1fe97
ashish <ashrestha@pcs.com.np>
2020-01-02 11:21:57 +0545

    Change in student

--- ReactDemo/ClientApp/src/components/Students.js
+++ ReactDemo/ClientApp/src/components/Students.js
@@ -1,11 +1,8 @@
 ﻿import React from 'react';
 import { connect } from 'react-redux';
-
-
 const Home = props => (
     <div>
         <h2>Student Registration</h2>
-       </div>
+    </div>
 );
-
 export default connect()(Home);
