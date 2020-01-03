0acc40793794d3ed8f63dc94aea891579745cee5
ashish <ashrestha@pcs.com.np>
2020-01-03 16:48:25 +0545

    change in students

--- ReactDemo/ClientApp/src/components/Students.js
+++ ReactDemo/ClientApp/src/components/Students.js
@@ -3,6 +3,30 @@ import { connect } from 'react-redux';
 const Home = props => (
     <div>
         <h2>Student Registration</h2>
+        <br />
+            <div className="col-md-4">
+            <label>Student Name: </label>
+            <input type="text" className="form-control" placeholder="xyzname"></input>
+            </div>
+            <div className="col-md-4">
+                <label>Address: </label>
+                <input type="text" className="form-control"></input>
+            </div>
+            <div className="col-md-4">
+                <label>Phone Number: </label>
+                <input type="text" className="form-control"></input>
+            </div>
+            <div className="col-md-4">
+                <label>Gender: </label>
+                <input type="text" className="form-control"></input>
+            </div>
+            <div className="col-md-4">
+                <label>Address: </label>
+                <input type="text" className="form-control"></input>
+        </div>
+        <div className="col-md-4">
+            <button class="btn btn-success">Add</button>
+        </div>
     </div>
 );
 export default connect()(Home);
